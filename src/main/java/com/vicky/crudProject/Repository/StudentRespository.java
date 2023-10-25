668ccacb1f00f73fa419a93f34f29110656bc889
A00477253 <vignesh.moorthy@smu.ca>
2023-10-25 11:30:48 -0300

    removing unused imports

--- src/main/java/com/vicky/crudProject/Repository/StudentRespository.java
+++ src/main/java/com/vicky/crudProject/Repository/StudentRespository.java
@@ -1,7 +1,6 @@
 package com.vicky.crudProject.Repository;
 
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.vicky.crudProject.Entity.Student;
